fb6e91d4cb180e620e99593f17d3608958c82f83
Rishab <rishabdakhale17@gmail.com>
2025-08-07 11:28:33 +0530

    fronted setup

---
+++ fronted/src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { isTokenExpired } from "./utils/checkToken";
+import DashBoard from "./pages/DashBoard";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token && isTokenExpired(token)) {
+      localStorage.removeItem("token");
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(!!token);
+    }
+  }, []);
+
+  if (isLoggedIn === null) return null;
+
+  return (
+    <>
+      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+      <Routes>
+        <Route
+          path="/"
+          element={isLoggedIn ? <DashBoard /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={
+            isLoggedIn ? (
+              <Navigate to="/" />
+            ) : (
+              <Login setIsLoggedIn={setIsLoggedIn} />
+            )
+          }
+        />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+        style={{
+          fontSize: "16px",
+          fontWeight: "bold",
+        }}
+      />
+    </>
+  );
+}
+
+export default App;
